35373e31cb94614a05fa7b10cbe4c20eceb399dc
thao.vu <thaovu@miu.edu>
2025-03-27 14:00:19 -0500

    added cors to be

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
+
 const app = express();
-require('dotenv').config();
+
+app.use(cors());
 app.use(express.json());
 
 // Configure the database connection
